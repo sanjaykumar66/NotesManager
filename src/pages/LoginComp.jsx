310f57a453db0a2acac8ea3e576e06f45fb20a9a
sanjaykumar66 <sanjayselva59@gmail.com>
2023-04-11 10:48:58 +0530

    logo changes

--- src/pages/LoginComp.jsx
+++ src/pages/LoginComp.jsx
@@ -25,7 +25,7 @@ function LoginComp(props) {
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img
             className="mx-auto h-12 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            src={`${process.env.PUBLIC_URL}/Logo.jpg`}
             alt="Your Company"
           />
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
